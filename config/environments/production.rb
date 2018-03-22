10f6d272a0296c7a27192a59f149e1c0d8b820e3
Russell Balest <russell@balest.com>
2018-03-22 12:06:06 -0400

    Get smtp creds from ENV in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,13 +74,13 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: ENV["SMTP_HOSTNAME"],
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_password
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"]
   }
   # ActionMailer Config
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
